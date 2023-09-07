278576f6037e6ca821f41f851fba49b66a199400
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-09-07 10:05:12 +0530

    import java.util.*; public class CurrentNumber {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<n;i++)         {             c=0;             for(int j=0;j<n;j++)             {                 if(a[i]>a[j])                 {                     c++;                 }             }             System.out.printf("%d ",c);         }     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class CurrentNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]>a[j])
+                {
+                    c++;
+                }
+            }
+            System.out.printf("%d ",c);
+        }
+    }
+}
