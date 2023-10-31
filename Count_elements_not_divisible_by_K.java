af49401df182405f664cf5e649fdda9737c3ae9b
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2023-10-31 21:41:58 +0530

    import java.util.Scanner; public class CountElementsNotDivisibleByK {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int k=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int c=0;         for(int i:arr)         {             if(i%k!=0)             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class CountElementsNotDivisibleByK
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i:arr)
+        {
+            if(i%k!=0)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
